a3ae7ccf96d34992b3551891b1091baaf9c3dcda
JoseOFF <jose000.jao@gmail.com>
2025-04-10 16:27:41 -0600

    Actualisacion de inicio de sesion

--- supabase-init.js
+++ supabase-init.js
@@ -5,4 +5,4 @@ const SUPABASE_URL = 'https://nfcfiuyqeoyefwxbrqlm.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mY2ZpdXlxZW95ZWZ3eGJycWxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQwODIyNTUsImV4cCI6MjA1OTY1ODI1NX0.hCIaMvRHfwMq6It5j0xFtbM1GJHZx7w-xLVFmfSIbhE';
 
 // Inicialización del cliente
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseCliente = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
